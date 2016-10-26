cc0b7ed32794bdb0ceab78fd843daf03f74a4168
Bartosz Taudul <bartosz.taudul@game-lion.com>
2016-10-26 15:01:40 +0200

    Add filesystem routines.

---
+++ src/Filesystem.cpp
@@ -0,0 +1,107 @@
+#include "Filesystem.hpp"
+
+#ifdef _WIN32
+#  include <direct.h>
+#  include <windows.h>
+#else
+#  include <dirent.h>
+#endif
+
+#include <string.h>
+
+bool CreateDirStruct( const std::string& path )
+{
+    if( Exists( path ) ) return true;
+
+    if( errno != ENOENT )
+    {
+        fprintf( stderr, "%s\n", strerror( errno ) );
+        return false;
+    }
+
+    size_t pos = 0;
+    do
+    {
+        pos = path.find( '/', pos+1 );
+#ifdef _WIN32
+        if( pos == 2 ) continue;    // Don't create drive name.
+        if( _mkdir( path.substr( 0, pos ).c_str() ) != 0 )
+#else
+        if( mkdir( path.substr( 0, pos ).c_str(), S_IRWXU ) != 0 )
+#endif
+        {
+            if( errno != EEXIST )
+            {
+                fprintf( stderr, "Creating failed on %s (%s)\n", path.substr( 0, pos ).c_str(), strerror( errno ) );
+                return false;
+            }
+        }
+    }
+    while( pos != std::string::npos );
+
+    return true;
+}
+
+std::unordered_set<std::string> ListDirectory( const std::string& path )
+{
+    std::unordered_set<std::string> ret;
+
+#ifdef _MSC_VER
+    WIN32_FIND_DATA ffd;
+    HANDLE h;
+
+    std::string p = path + "/*";
+    for( unsigned int i=0; i<p.size(); i++ )
+    {
+        if( p[i] == '/' )
+        {
+            p[i] = '\\';
+        }
+    }
+
+    h = FindFirstFile( ( p ).c_str(), &ffd );
+    if( h == INVALID_HANDLE_VALUE )
+    {
+        return ret;
+    }
+
+    do
+    {
+        std::string s = ffd.cFileName;
+        if( s != "." && s != ".." )
+        {
+            if( ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY )
+            {
+                s += "/";
+            }
+            ret.emplace( std::move( s ) );
+        }
+    }
+    while( FindNextFile( h, &ffd ) );
+
+    FindClose( h );
+#else
+    DIR* dir = opendir( path.c_str() );
+    if( dir == nullptr )
+    {
+        return ret;
+    }
+
+    struct dirent* ent;
+    while( ( ent = readdir( dir ) ) != nullptr )
+    {
+        std::string s = ent->d_name;
+        if( s != "." && s != ".." )
+        {
+            if( ent->d_type == DT_DIR )
+            {
+                s += "/";
+            }
+            ret.emplace( std::move( s ) );
+        }
+    }
+    closedir( dir );
+#endif
+
+    return ret;
+}
---
+++ src/Filesystem.hpp
@@ -0,0 +1,43 @@
+#ifndef __FILESYSTEM_HPP__
+#define __FILESYSTEM_HPP__
+
+#include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unordered_set>
+
+bool CreateDirStruct( const std::string& path );
+std::unordered_set<std::string> ListDirectory( const std::string& path );
+
+#ifdef _MSC_VER
+#  define stat64 _stat64
+#endif
+#ifdef __CYGWIN__
+#  define stat64 stat
+#endif
+
+#ifndef S_ISREG
+#  define S_ISREG(m) ( ( (m) & S_IFMT ) == S_IFREG )
+#endif
+
+static inline bool Exists( const std::string& path )
+{
+    struct stat64 buf;
+    return stat64( path.c_str(), &buf ) == 0;
+}
+
+static inline bool IsFile( const std::string& path )
+{
+    struct stat64 buf;
+    stat64( path.c_str(), &buf );
+    return S_ISREG( buf.st_mode );
+}
+
+static inline uint64_t GetFileSize( const char* path )
+{
+    struct stat64 buf;
+    stat64( path, &buf );
+    return buf.st_size;
+}
+
+#endif
